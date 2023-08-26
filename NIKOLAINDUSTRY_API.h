71e01df0263c9827f66cc97bb16d77b098d12968
Mayur Sunil Chavan <98825289+mayurtechnologys@users.noreply.github.com>
2023-08-26 10:31:15 +0530

    Update NIKOLAINDUSTRY_API.h

--- NIKOLAINDUSTRY_API.h
+++ NIKOLAINDUSTRY_API.h
@@ -7,10 +7,10 @@
 class NIKOLAINDUSTRY_API {
 public:
     NIKOLAINDUSTRY_API();
-    String writeDataToDatabase();
-    String readDataFromDatabase();
-    String updateDataInDatabase();
-    String readAllDatabaseData();
+    String writeDataToDatabase(const char* databaseKey, const char* userId, const char* itemId);
+    String readDataFromDatabase(const char* databaseKey, const char* itemId);
+    String updateDataInDatabase(const char* databaseKey, const char* userId, const char* itemId);
+    String readAllDatabaseData(const char* databaseKey);
 private:
     HTTPClient http;
 };
