36ae64dba6df36eb96b11c92e9651b878b12e5d2
Mayur Sunil Chavan <98825289+mayurtechnologys@users.noreply.github.com>
2023-08-28 15:17:29 +0530

    Update and rename NIKOLAINDUSTRYAPI.ino to NIKOLAINDUSTRYDBMSExample.ino

---
+++ NIKOLAINDUSTRYDBMSExample.ino
@@ -0,0 +1,45 @@
+#include <WiFi.h>
+#include "NIKOLAINDUSTRY_DBMS.h"
+
+
+const char* ssid = "NIKOLA";        // Replace with your network SSID
+const char* password = "12345678";  // Replace with your network password
+
+String userid = "mayurtechnologys16@gmail.com";
+NIKOLAINDUSTRY_DBMS dbms(userid);
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  // Check if connected to Wi-Fi before sending data
+  
+  if (WiFi.status() == WL_CONNECTED) {
+    String data = "&Name=Mayur&RollNo=14&PhoneNo=1111111111";
+    // Example: Writing data
+    String writeResponse = dbms.WriteData("fXr6xXPGlGJXivh", "HrY1Y", data);
+    Serial.println("Write Response: " + writeResponse);
+
+    // Example: Reading data
+    String readResponse = dbms.ReadData("fXr6xXPGlGJXivh", "HrY1Y");
+    Serial.println("Read Response: " + readResponse);
+
+    // Example: Update data
+    String updateddata = "&Name=Rupesh&RollNo=13&PhoneNo=888888888";
+    String updatedResponse = dbms.UpdateData("fXr6xXPGlGJXivh", "HrY1Y", updateddata);
+    Serial.println("Read updated Response: " + updatedResponse);
+
+    // Example: Read All data
+    String readallResponse = dbms.ReadAllitems("fXr6xXPGlGJXivh");
+    Serial.println(" Read All Response: " + readallResponse);
+
+    delay(3000);
+  }
+}
